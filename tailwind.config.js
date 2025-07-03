526f377af8d0740b82a9b8a3765c40f5585c80aa
Sanjay <Sanjusuthar1599@gmail.com>
2025-07-03 14:28:07 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js (ya .ts agar TS use kar rahe ho)
+
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
